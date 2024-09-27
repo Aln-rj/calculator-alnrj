453e26bb21fe7898b63306a7a923ebbfae15422a
Aln-rj <alanraj416@gmail.com>
2024-09-27 15:56:44 +0530

    index

---
+++ index.js
@@ -0,0 +1,9 @@
+function displaycontent(content){
+    result.value += content
+}
+function clearcalcscreen(){
+    result.value = ''
+}
+function displayresult(){
+result.value = eval(result.value)
+}
